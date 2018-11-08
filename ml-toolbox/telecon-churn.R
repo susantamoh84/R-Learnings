d9dad0195e44ad0425b668c0faad878c5f1e1660
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-08 17:26:47 +0530

    Create telecon-churn.R

---
+++ ml-toolbox/telecon-churn.R
@@ -0,0 +1,11 @@
+# Create custom indices: myFolds
+myFolds <- createFolds(churn_y, k = 5)
+
+# Create reusable trainControl object: myControl
+myControl <- trainControl(
+  summaryFunction = twoClassSummary,
+  classProbs = TRUE, # IMPORTANT!
+  verboseIter = TRUE,
+  savePredictions = TRUE,
+  index = myFolds
+)
