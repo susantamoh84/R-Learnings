dac408fc41e812a6a8448cba93b5ef83732c2338
susantamoh84 <42180526+susantamoh84@users.noreply.github.com>
2018-11-08 17:41:31 +0530

    Update telecon-churn.R

--- ml-toolbox/telecon-churn.R
+++ ml-toolbox/telecon-churn.R
@@ -9,3 +9,41 @@ myControl <- trainControl(
   savePredictions = TRUE,
   index = myFolds
 )
+
+# Fit glmnet model: model_glmnet
+model_glmnet <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "glmnet",
+  trControl = myControl
+)
+
+# Fit random forest: model_rf
+model_rf <- train(
+  x = churn_x, y = churn_y,
+  metric = "ROC",
+  method = "ranger",
+  trControl = myControl
+)
+
+# Create model_list
+model_list <- list(item1 = model_glmnet, item2 = model_rf)
+
+# Pass model_list to resamples(): resamples
+resamples <- resamples(model_list)
+
+# Summarize the results
+summary(resamples)
+
+# Create bwplot
+bwplot(resamples, metric="ROC")
+
+# Create xyplot
+xyplot(resamples, metric="ROC")
+
+# Create ensemble model: stack
+stack <- caretStack(model_list, method="glm")
+
+# Look at summary
+summary(stack)
+
